ca1b04ed238b77771553f6aa32c726fe748c9edb
Santiago AV <dev3@tropicalhouserecords.com>
2026-02-02 00:35:09 -0700

    Remove unused ContactForm component from App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import WhyHireMe from './components/WhyHireMe';
 import Timeline from './components/Timeline';
-import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import { Toaster } from './components/ui/toaster';
 
@@ -20,7 +19,6 @@ function App() {
           <div id="about"></div>
           <WhyHireMe />
           <Timeline />
-          <ContactForm />
         </main>
         <Footer />
         <Toaster />
